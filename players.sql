42c48a5e5bcea6917b2eed8e9897ed3314e5600b
markaigbe <72993750+markaigbe@users.noreply.github.com>
2021-04-26 21:25:57 +0100

    Add files via upload

---
+++ players.sql
@@ -0,0 +1,48 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               5.7.24 - MySQL Community Server (GPL)
+-- Server OS:                    Win64
+-- HeidiSQL Version:             10.2.0.5599
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Dumping data for table mgp.players: ~26 rows (approximately)
+/*!40000 ALTER TABLE `players` DISABLE KEYS */;
+INSERT IGNORE INTO `players` (`id`, `player`) VALUES
+	(1, '------------------------'),
+	(2, 'Bruno Fernandes'),
+	(3, 'Aaron Wan-Bissaka'),
+	(4, 'Marcus Rashford'),
+	(5, 'David de Gea'),
+	(6, 'Victor Lindelöf'),
+	(7, 'Luke Shaw'),
+	(8, 'Paul Pogba'),
+	(9, 'Fred'),
+	(10, 'Anthony Martial'),
+	(11, 'Scott McTominay'),
+	(12, 'Mason Greenwood'),
+	(13, 'Nemanja Matic'),
+	(14, 'Edinson Cavani'),
+	(15, 'Eric Bailly'),
+	(16, 'Alex Telles'),
+	(17, 'Daniel James'),
+	(18, 'Juan Mata'),
+	(19, 'Donny van de Beek'),
+	(20, 'Dean Henderson'),
+	(21, 'Axel Tuanzebe'),
+	(22, 'Timothy Fosu-Mensah'),
+	(23, 'Odion Ighalo'),
+	(24, 'Brandon Williams'),
+	(25, 'Lee Grant'),
+	(26, 'Jesse Lingard'),
+	(27, 'Harry Maguire');
+/*!40000 ALTER TABLE `players` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
